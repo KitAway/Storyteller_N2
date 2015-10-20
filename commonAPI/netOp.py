6700c076b85a3b670e1040bd684b6770dbc608c1
KitAway <KitAway>
2015-10-20 12:36:50 +0200

    client multilanguge added

--- commonAPI/netOp.py
+++ commonAPI/netOp.py
@@ -15,14 +15,16 @@ import http.client
 
 
 def httpPOST(url,data,headers):
-    # throw TimeoutError, ConnectionRefusedError#
     if not type(data) is bytes:
         data=data.encode('utf-8')
-    conn = http.client.HTTPConnection(url)
-    conn.request("POST", "", data, headers)
-    response = conn.getresponse()
-    return response
-
+        
+    try:    
+        conn = http.client.HTTPConnection(url)
+        conn.request("POST", "", data, headers)
+        response = conn.getresponse()
+        return response
+    except Exception as e:
+        raise e
 
 def httpsPOST(url,data,headers):
     # throw TimeoutError, ConnectionRefusedError#
@@ -34,7 +36,10 @@ def httpsPOST(url,data,headers):
     return response
 
 def httpGET(url,pos,hr={}):
-    conn = http.client.HTTPConnection(url)
-    conn.request("GET", "%s"%pos,'',hr)
-    response = conn.getresponse()
-    return response
+    try:
+        conn = http.client.HTTPConnection(url)
+        conn.request("GET", "%s"%pos,'',hr)
+        response = conn.getresponse()
+        return response
+    except Exception as e:
+        raise e
